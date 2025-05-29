07f66bc80a09991d7f5acc0a7d99212161261371
qus0in <qus0in@gmail.com>
2025-05-29 11:34:54 +0900

    Initial commit

---
+++ src/main/java/org/example/bootcloudstep3/BootCloudStep3Application.java
@@ -0,0 +1,13 @@
+package org.example.bootcloudstep3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCloudStep3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCloudStep3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootcloudstep3/BootCloudStep3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootcloudstep3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCloudStep3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
